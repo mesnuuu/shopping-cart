48e7e110db12e03dad92dc53869c93187e241ccd
Muhammad Saleem <46952459+mesnuuu@users.noreply.github.com>
2023-03-05 16:21:03 +0500

    Updating store with useDispatch hook

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,43 @@
 import React from 'react'
 import '../styles/home.scss'
 import productList from '../data/productList'
+import { useDispatch } from 'react-redux'
+import cartSlice from '../data/cartSlice'
 
 
 const Home = () => {
-    return (
-        <div className="container product-catalogue">
-        <div className="row">
-          {productList.products.map((product) => {
-            return (
-              <div className="wrapper col-md-4" key={product.id}>
-                <div className="card">
-                  <img className="card-img-top center-block" src={product.imageUrl} alt="Card cap" />
-  
-                  <div className="card-body text-center">
-                    <h5 className="card-title">{product.name}</h5>
-                    <p className="card-text">${product.price}</p>
-  
-                    <button className="btn btn-primary">Add to cart</button>
-                  </div>
+
+  const { addToCart, removeFromCart } = cartSlice.actions
+  const dispatch = useDispatch()
+
+  return (
+    <div className="container product-catalogue">
+      <div className="row">
+        {productList.products.map((product) => {
+          return (
+            <div className="wrapper col-md-4" key={product.id}>
+              <div className="card">
+                <img className="card-img-top center-block" src={product.imageUrl} alt="Card cap" />
+
+                <div className="card-body text-center">
+                  <h5 className="card-title">{product.name}</h5>
+                  <p className="card-text">${product.price}</p>
+
+                  <button className="btn btn-primary" onClick={() => dispatch(addToCart(product.id))}>
+                    Add to cart
+                  </button>
+                  <button className="btn btn-primary" onClick={() => dispatch(removeFromCart(product.id))}>
+                    Remove from cart
+                  </button>
+
                 </div>
               </div>
-            )
-          })}
-        </div>
+            </div>
+          )
+        })}
       </div>
-    )
+    </div>
+  )
 }
 
 export default Home
