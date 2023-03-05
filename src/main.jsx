f022744e8f772974ce324499c32fd4f651a6b248
Muhammad Saleem <46952459+mesnuuu@users.noreply.github.com>
2023-03-05 15:45:50 +0500

    Setting up redux toolkit and store

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/index.scss'
+import { Provider } from 'react-redux'
+import store from './data/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
 )
