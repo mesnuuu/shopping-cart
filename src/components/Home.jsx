e43e2579c162f06a58ac31c4a25dd9180ffd6ad0
Muhammad Saleem <46952459+mesnuuu@users.noreply.github.com>
2023-03-05 16:29:03 +0500

    Getting data from store using useDispatch hook

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import '../styles/home.scss'
 import productList from '../data/productList'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import cartSlice from '../data/cartSlice'
 
 
 const Home = () => {
 
+  const { cartProductIds } = useSelector((state) => state.cart)
   const { addToCart, removeFromCart } = cartSlice.actions
   const dispatch = useDispatch()
+
 
   return (
     <div className="container product-catalogue">
@@ -23,12 +25,16 @@ const Home = () => {
                   <h5 className="card-title">{product.name}</h5>
                   <p className="card-text">${product.price}</p>
 
-                  <button className="btn btn-primary" onClick={() => dispatch(addToCart(product.id))}>
-                    Add to cart
-                  </button>
-                  <button className="btn btn-primary" onClick={() => dispatch(removeFromCart(product.id))}>
-                    Remove from cart
-                  </button>
+                  {!cartProductIds.includes(product.id) && (
+                    <button className="btn btn-primary" onClick={() => dispatch(addToCart(product.id))}>
+                      Add to cart
+                    </button>
+                  )}
+                  {cartProductIds.includes(product.id) && (
+                    <button className="btn btn-primary" onClick={() => dispatch(removeFromCart(product.id))}>
+                      Remove from cart
+                    </button>
+                  )}
 
                 </div>
               </div>
